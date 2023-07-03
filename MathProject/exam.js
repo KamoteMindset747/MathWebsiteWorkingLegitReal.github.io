ba6c8c6f3cbbd6e3b257c1338f3274e04a0e3a56
KamoteCodeRil <132143568+KamoteMindset747@users.noreply.github.com>
2023-07-03 19:45:25 +0800

    Add files via upload

---
+++ MathProject/exam.js
@@ -0,0 +1,172 @@
+function Submitter(){
+    let Name = document.querySelector(".Int1").value;
+
+while(Name !== "55.77")
+{
+    alert("Wrong");
+    return;
+}
+
+alert("Correct! the answer is " + Name);
+}
+
+function Submitter2(){
+    let Name2 = document.querySelector(".Int2").value;
+
+    while(Name2 !== "13.23")
+    {
+        alert("Wrong");
+        return;
+    }
+    
+    alert("Correct! the answer is " + Name2);
+}
+
+function Submitter3(){
+    let Name3 = document.querySelector(".Int3").value;
+
+    while(Name3 !== "34.23")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name3);
+}
+
+function Submitter4(){
+    let Name4 = document.querySelector(".Int4").value;
+
+    while(Name4 !== "43")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name4);
+}
+
+function Submitter5(){
+    let Name5 = document.querySelector(".Int5").value;
+
+    while(Name5 !== "7.46")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name5);
+}
+
+function Submitter6(){
+    let Name6 = document.querySelector(".Int6").value;
+
+    while(Name6 !== "10.94")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name6);
+}
+
+function Submitter7(){
+    let Name7 = document.querySelector(".Int7").value;
+
+    while(Name7 !== "10.46")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name7);
+}
+
+function Submitter8(){
+    let Name8 = document.querySelector(".Int8").value;
+
+    while(Name8 !== "10.46")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name8);
+}
+function Submitter9(){
+    let Name9 = document.querySelector(".Int9").value;
+
+    while(Name9 !== "55")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name9);
+}
+function Submitter10(){
+    let Name10 = document.querySelector(".Int10").value;
+
+    while(Name10 !== "54.39")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name10);
+}
+function Submitter11(){
+    let Name11 = document.querySelector(".Int11").value;
+
+    while(Name11 !== "67.61")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name11);
+}
+function Submitter12(){
+    let Name12 = document.querySelector(".Int12").value;
+
+    while(Name12 !== "6.78")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name12);
+}
+function Submitter13(){
+    let Name13 = document.querySelector(".Int13").value;
+
+    while(Name13 !== "3.19")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name13);
+}
+function Submitter14(){
+    let Name14 = document.querySelector(".Int14").value;
+
+    while(Name14 !== "9.19")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name14);
+}
+function Submitter15(){
+    let Name15 = document.querySelector(".Int15").value;
+
+    while(Name15 !== "23.81")
+    {
+        alert("Wrong");
+        return;
+    }
+
+    alert("Correct! the answer is " + Name15);
+}
